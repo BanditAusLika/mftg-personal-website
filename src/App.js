228e04c06c224b07b2481bb7bc08028f488ae6de
El Nomadovich <ai.de.f2020@gmail.com>
2025-01-24 16:54:21 +0100

    Resolved merge conflicts, updated dependencies, and regenerated package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -30,30 +29,5 @@ const App = () => (
     <Footer />
   </>
 );
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
->>>>>>> 622e934 (Initialize project using Create React App)
 
 export default App;
